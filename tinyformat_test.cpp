8448b43946db35e8f58b55424f2de229701e8973
Will Wray <wjwray@gmail.com>
2019-04-04 16:32:44 -0400

    Add support for %a,%A C99 format specs for hexfloat
    Add tests for formatted output, width modifier and precision (NA).
    Fix two typos.

--- tinyformat_test.cpp
+++ tinyformat_test.cpp
@@ -122,6 +122,8 @@ int unitTests()
     CHECK_EQUAL(tfm::format("%E", -1.23456E10), "-1.234560E+10");
     CHECK_EQUAL(tfm::format("%f", -9.8765), "-9.876500");
     CHECK_EQUAL(tfm::format("%F", 9.8765), "9.876500");
+    CHECK_EQUAL(tfm::format("%a", -9.75), "-0x1.38p+3");
+    CHECK_EQUAL(tfm::format("%A", 9.75), "0X1.38P+3");
     CHECK_EQUAL(tfm::format("%g", 10), "10");
     CHECK_EQUAL(tfm::format("%G", 100), "100");
     CHECK_EQUAL(tfm::format("%c", 65), "A");
@@ -163,6 +165,8 @@ int unitTests()
     CHECK_EQUAL(tfm::format("%.4d", 10), "0010");
     CHECK_EQUAL(tfm::format("%10.4f", 1234.1234567890), " 1234.1235");
     CHECK_EQUAL(tfm::format("%.f", 10.1), "10");
+    CHECK_EQUAL(tfm::format("%.a", -9.75), "-0x1.38p+3"); // precision ignored for hex float as required
+    CHECK_EQUAL(tfm::format("%10a", 9.75), " 0x1.38p+3");
     CHECK_EQUAL(tfm::format("%.2s", "asdf"), "as"); // strings truncate to precision
     CHECK_EQUAL(tfm::format("%.2s", std::string("asdf")), "as");
     // Test variable precision & width
@@ -245,8 +249,6 @@ int unitTests()
 
     // Unhandled C99 format spec
     EXPECT_ERROR( tfm::format("%n", 10) )
-    EXPECT_ERROR( tfm::format("%a", 10) )
-    EXPECT_ERROR( tfm::format("%A", 10) )
 
 #ifdef TEST_WCHAR_T_COMPILE
     // Test wchar_t handling - should fail to compile!
